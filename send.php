3194144f93a88f9571ded95059728be152a5c7fe
Артём Михалькевич <mihalkevichartiom@gmail.com>
2022-12-24 15:49:36 +0300

    zaza

--- send.php
+++ send.php
@@ -1,24 +1,4 @@
 <?php
-$name = $_POST['name']
-echo $name
-// use PHPMailer\PHPMailer\PHPMailer;
-// use PHPMailer\PHPMailer\Exception;
-
-// require 'phpmailer/src/Exception.php';
-// require 'phpmailer/src/PHPMailer.php';
-// require 'phpmailer/src/SMTP.php';
-
-// $mail = new PHPMailer(true);
-
-// $mail->isSMTP();
-// $mail->Host = 'smtp.gmail.com';
-// $mail->SMTPAuth = true;
-// $mail->Username = 'lexlehovich@gmail.com';
-// $mail->Password = 'mfqdxlbbaywewuqb'
-// $mail->SMTPSecure = 'ssl';
-// $mail->Port = 465;
-// $mail->setFrom('lexlehovich@gmail.com');
-// $mail->addAddress('mihalkevichartiom@gmail.com')
-// $mail ->Body = $name;
-// $mail->send();
-// ?>
+$name = $_POST['name'];
+echo $name;
+?>

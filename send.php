4b83869212ebd508a6379a0c1f994d51a2c9ec94
Артём Михалькевич <mihalkevichartiom@gmail.com>
2022-12-24 15:18:24 +0300

    333

--- send.php
+++ send.php
@@ -1,7 +1,6 @@
 <?php
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
-use PHPMailer\PHPMailer\SMTP;
 
 require 'phpmailer/src/Exception.php';
 require 'phpmailer/src/PHPMailer.php';

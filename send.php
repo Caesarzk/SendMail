9936f23f4c5f4cc0aa0973a4823facb920c7bfe7
Артём Михалькевич <mihalkevichartiom@gmail.com>
2022-12-24 15:38:03 +0300

    orig

--- send.php
+++ send.php
@@ -1,22 +1,24 @@
 <?php
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\Exception;
+$name = $_POST['name']
+echo $name
+// use PHPMailer\PHPMailer\PHPMailer;
+// use PHPMailer\PHPMailer\Exception;
 
-require 'phpmailer/src/Exception.php';
-require 'phpmailer/src/PHPMailer.php';
-require 'phpmailer/src/SMTP.php';
+// require 'phpmailer/src/Exception.php';
+// require 'phpmailer/src/PHPMailer.php';
+// require 'phpmailer/src/SMTP.php';
 
-$mail = new PHPMailer(true);
+// $mail = new PHPMailer(true);
 
-$mail->isSMTP();
-$mail->Host = 'smtp.gmail.com';
-$mail->SMTPAuth = true;
-$mail->Username = 'lexlehovich@gmail.com';
-$mail->Password = 'mfqdxlbbaywewuqb'
-$mail->SMTPSecure = 'ssl';
-$mail->Port = 465;
-$mail->setFrom('lexlehovich@gmail.com');
-$mail->addAddress('mihalkevichartiom@gmail.com')
-$mail ->Body = $_POST["name"];
-$mail->send();
-?>
+// $mail->isSMTP();
+// $mail->Host = 'smtp.gmail.com';
+// $mail->SMTPAuth = true;
+// $mail->Username = 'lexlehovich@gmail.com';
+// $mail->Password = 'mfqdxlbbaywewuqb'
+// $mail->SMTPSecure = 'ssl';
+// $mail->Port = 465;
+// $mail->setFrom('lexlehovich@gmail.com');
+// $mail->addAddress('mihalkevichartiom@gmail.com')
+// $mail ->Body = $name;
+// $mail->send();
+// ?>
